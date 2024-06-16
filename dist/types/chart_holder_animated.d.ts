656c815e47028e9d4479ad2a94219ee78de5dd31
timroberton <timroberton@gmail.com>
2024-06-16 20:16:14 +0800

    Update at 2024_06_16_20_16_14

--- dist/types/chart_holder_animated.d.ts
+++
@@ -1,9 +0,0 @@
-import { RectCoordsDims, TimChartInputs } from "@jsr/timroberton__panther";
-type Props<T extends Object> = {
-    getChartInputs: (obj: T) => TimChartInputs;
-    getChartRcd?: (obj: T) => RectCoordsDims;
-    obj: T;
-    aspectRatio?: number;
-};
-export declare function ChartHolderAnimated<T extends Object>(p: Props<T>): import("solid-js").JSX.Element;
-export {};
--- dist/types/checkbox.d.ts
+++
@@ -1,8 +0,0 @@
-import { CheckboxRootProps } from "@kobalte/core/checkbox";
-type Props = {
-    checked: CheckboxRootProps["checked"];
-    onChange?: CheckboxRootProps["onChange"];
-    label: string;
-};
-export declare function Checkbox(p: Props): import("solid-js").JSX.Element;
-export {};
