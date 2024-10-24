994e5acb05a8760b75b1fbce9b96a48150534225
timroberton <timroberton@gmail.com>
2024-10-24 09:54:06 +0800

    Update at 2024_10_24_09_54_06

--- dist/timroberton-panther/_00_utils/sort.d.ts
+++ dist/timroberton-panther/_00_utils/sort.d.ts
@@ -2,3 +2,5 @@ export declare function getSortedAlphabetical(arr: string[]): string[];
 export declare function sortAlphabetical(arr: string[]): void;
 export declare function sortFuncAlphabetical(a: string, b: string): number;
 export declare function sortFuncAlphabeticalReverse(b: string, a: string): number;
+export declare function getSortedAlphabeticalByFunc<T>(arr: T[], byFunc: (v: T) => string): T[];
+export declare function sortAlphabeticalByFunc<T>(arr: T[], byFunc: (v: T) => string): void;
--- dist/timroberton-panther/_00_utils/sort.js
+++ dist/timroberton-panther/_00_utils/sort.js
@@ -28,3 +28,36 @@ export function sortFuncAlphabeticalReverse(b, a) {
     }
     return 0;
 }
+///////////////////////////////////////////////////////////////////////
+//  _______                    ______                                //
+// /       \                  /      \                               //
+// $$$$$$$  | __    __       /$$$$$$  |__    __  _______    _______  //
+// $$ |__$$ |/  |  /  |      $$ |_ $$//  |  /  |/       \  /       | //
+// $$    $$< $$ |  $$ |      $$   |   $$ |  $$ |$$$$$$$  |/$$$$$$$/  //
+// $$$$$$$  |$$ |  $$ |      $$$$/    $$ |  $$ |$$ |  $$ |$$ |       //
+// $$ |__$$ |$$ \__$$ |      $$ |     $$ \__$$ |$$ |  $$ |$$ \_____  //
+// $$    $$/ $$    $$ |      $$ |     $$    $$/ $$ |  $$ |$$       | //
+// $$$$$$$/   $$$$$$$ |      $$/       $$$$$$/  $$/   $$/  $$$$$$$/  //
+//           /  \__$$ |                                              //
+//           $$    $$/                                               //
+//            $$$$$$/                                                //
+//                                                                   //
+///////////////////////////////////////////////////////////////////////
+export function getSortedAlphabeticalByFunc(arr, byFunc) {
+    const newArr = [...arr];
+    sortAlphabeticalByFunc(newArr, byFunc);
+    return newArr;
+}
+export function sortAlphabeticalByFunc(arr, byFunc) {
+    arr.sort((a, b) => {
+        const a1 = byFunc(a).toLowerCase().trim();
+        const b1 = byFunc(b).toLowerCase().trim();
+        if (a1 < b1) {
+            return -1;
+        }
+        if (a1 > b1) {
+            return 1;
+        }
+        return 0;
+    });
+}
